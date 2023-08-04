3247581513a19dba2297781cc7a610af7f588a70
Decoder0007 <decoder00007@gmail.com>
2023-08-04 07:27:55 -0700

    Stay Focused. Be Committed

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from git import Repo
+from tqdm import tqdm
 
 PATH_OF_GIT_REPO = r'/Users/decoder0007/Desktop/Dedicated/.git'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'Stay Focused. Be Committed'
 
 def dedicate():
+    print("Getting Repository...")
     repo = Repo(PATH_OF_GIT_REPO)
+    print("Adding Files...")
     repo.git.add(update=True)
-    for i in range(99):
+    print("Generating Commits...")
+    for i in tqdm(range(900)):
         repo.index.commit(COMMIT_MESSAGE)
+    print("Retrieving Remote...")
     origin = repo.remote(name='origin')
+    print("Pushing...")
     origin.push()
+    print("Finish")
 
 dedicate()
