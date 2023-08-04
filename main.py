fd7aeaf92f9a25213336f39439b0c4bbd80fc102
Decoder0007 <decoder00007@gmail.com>
2023-08-04 15:34:25 +0100

    Stay Focused. Be Committed

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from git import Repo
 from tqdm import tqdm
-
-# MacOS: r'/Users/decoder0007/Desktop/Dedicated/.git'
-# Windows: r'C:/Users/decod/Desktop/Dedicated/.git'
+import os
 
 PATH_OF_GIT_REPO = r'C:/Users/decod/Desktop/Dedicated/.git'
 
 def dedicate():
-    commits = int(input("Enter number of commits to push:  "))
+    os.system("cls")
     print("Getting Repository...")
     repo = Repo(PATH_OF_GIT_REPO)
     print("Adding Files...")
     repo.git.add(update=True)
-    print("Generating Commits...")
-    for i in tqdm(range(commits)):
-        repo.index.commit('Stay Focused. Be Committed')
-    print("Retrieving Remote...")
-    origin = repo.remote(name='origin')
-    print("Pushing...")
-    origin.push()
+    while True:
+        os.system("cls")
+        print("Generating Commits...")
+        for i in tqdm(range(10000)):
+            repo.index.commit('Stay Focused. Be Committed')
+        print("Retrieving Remote...")
+        origin = repo.remote(name='origin')
+        print("Pushing...")
+        origin.push()
     print("Finish")
 
 dedicate()
